dfddc24306f9c26cd323e916b39d33e1f728801c
Bruno Azuara <bruno.azuara@gmail.com>
2021-07-17 17:58:30 +0200

    protected arg input

--- main.rb
+++ main.rb
@@ -15,25 +15,30 @@ rescue StandardError => err
   p "Wrong secret.credentials.yml file, are you sure it exist and is formated ok?"
 end
 
-ARGV.each_with_index do |value, i|
-  p "ARG #{i} #{value}"
+# Protect arg input
+unless (ARGV.first == nil || ARGV.length > 1)
+  user = ARGV.first
+else
+  p "Usage: You need to pass a single valid login as argument"
+  exit(1)
 end
-
-user = ARGV.first
 
 # Check credentials
 begin
   client = OAuth2::Client.new(client_id, client_secret, site: "https://api.intra.42.fr")
   token = client.client_credentials.get_token
-  answer = token.get("/v2/users/#{user}")
+  response = token.get("/v2/users/#{user}")
 rescue StandardError => err
   p "Rescue @Check_api_credentials #{err.inspect}"
   p "Wrong secret.credentials.yml content, please check your credentials"
 end
-if answer.status != 200
-  p "Sorry, something went wrong with the API, try it later or report the problem"
-  exit
+
+#check response status
+if response.status != 200
+  p "Sorry, something went wrong with the API, try again later or report the problem"
+  exit(2)
 end
-user_data = answer.parsed
-p answer.status
+user_data = response.parsed
+p "Debug"
+p response.status
 p user_data['usual_full_name']
